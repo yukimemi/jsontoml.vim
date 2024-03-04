8e127e5845fe19aeeea1c78086ab9f513c95859b
GitHub <noreply@github.com>
2024-03-04 06:22:28 +0000

    chore(deps): Update deno dependencies

--- denops/jsontoml/main.ts
+++ denops/jsontoml/main.ts
@@ -1,7 +1,7 @@
 import * as TOML from "https://deno.land/std@0.218.2/toml/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v6.2.0/function/mod.ts";
-import * as opt from "https://deno.land/x/denops_std@v6.2.0/option/mod.ts";
-import type { Denops } from "https://deno.land/x/denops_std@v6.2.0/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v6.3.0/function/mod.ts";
+import * as opt from "https://deno.land/x/denops_std@v6.3.0/option/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v6.3.0/mod.ts";
 import { assert, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
 
 // from https://qiita.com/usoda/items/dbedc06fd4bf38a59c48
